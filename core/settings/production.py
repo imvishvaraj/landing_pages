a9aa8b9f32e95daccf7f7c20558250ec62212b5d
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-20 17:05:24 +0530

    heroku deployment settings and updated production file

--- core/settings/production.py
+++ core/settings/production.py
@@ -27,7 +27,7 @@ SECRET_KEY = '(c82re30#ght^z49*^8jjnik_(3gjeeq4l!x&sox8nwvs-!yuu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mininews.heroku.com']
 
 
 # Application definition
