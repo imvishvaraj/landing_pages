abc0f8b64042a66e4fc61f843ebe85effdbd2abb
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-26 18:43:33 +0530

    Page model and dynamic content

--- pages/views.py
+++ pages/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic import View, FormView, CreateView
 
 from newsletter.forms import JoinForm
+from .models import Page
 
 # class HomeView(View):
 #     def get(self, request, *args, **kwargs):
@@ -13,6 +14,12 @@ class HomeView(SuccessMessageMixin, CreateView):
     template_name = 'pages/home.html'
     form_class = JoinForm
     success_url = '/'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        context["page_obj"] = Page.objects.all().order_by("?").first()
+        return context
+    
 
     def get_success_message(self, cleaned_data):
         print(cleaned_data)
