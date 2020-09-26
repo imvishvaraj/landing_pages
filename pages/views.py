5f7765e69f863f8a88562425857a0b1ba5ed89a9
Vishvaraj Dhanawade <vishvarajdhnwd120@gmail.com>
2020-09-26 17:33:18 +0530

    added custome model to capture emails and integrated in site

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import View
+from django.views.generic import View, FormView
+
+from newsletter.forms import JoinForm
+
+# class HomeView(View):
+#     def get(self, request, *args, **kwargs):
+#         return render(request, "pages/home.html", {})
 
 
-class HomeView(View):
-    def get(self, request, *args, **kwargs):
-        return render(request, "pages/home.html", {})
+class HomeView(FormView):
+    template_name = 'pages/home.html'
+    form_class = JoinForm
+    success_url = '/'
